99999797089fb13e0bf7d85e078555acebef8d1d
ElangovanPro <elangovanvcet@gmail.com>
2025-08-13 11:09:18 -0500

    parcel

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   {
-//     style: {
-//       color: "blue",
-//       fontSize: "24px",
-//       backgroundColor: "lightgray"
-//     }
-//   },
-//   "Hello world"
-// );
-
 const heading = React.createElement(
   "div",
   {id:"parent"},
   React.createElement("h1",{id:"first child"},"Hello world"),
-  React.createElement("h2",{id:"second child", style:{background:"red"}},"Hello world")
+  React.createElement("h3",{id:"second child"},"Welcome to the React World")
 );
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);//its converts the js object or react element to the html format which browser understands(browser doesn't understand react direclty)
