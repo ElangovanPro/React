817919fc4507a8ec61882da09f02e84ac6dd5800
ElangovanPro <elangovanvcet@gmail.com>
2025-08-15 09:59:57 -0500

    How React,babel,jsx,tsx,js,ts

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "div",
-  {id:"parent"},
-  React.createElement("h1",{id:"first child"},"Hello world"),
-  React.createElement("h3",{id:"second child"},"Welcome to the React World")
-);
+const heading=(
+<div>
+<h1>hello world</h1>
+<h3>Welcome to the React World</h3>
+{100+200}
+</div>);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);//its converts the js object or react element to the html format which browser understands(browser doesn't understand react direclty)
 console.log(heading);//object
