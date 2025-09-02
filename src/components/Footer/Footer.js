f6c6c03ab675b953bc8ba444de0cc198c01b0d62
ElangovanPro <elangovanvcet@gmail.com>
2025-09-02 14:16:46 -0500

    react hooks-useState

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './Footer.css';
 
 
@@ -6,12 +7,26 @@ export const footerLink="https://www.google.com";
 
 export const footerLink1="https://www.google.com";
 
+// let age=18;
+
 
 //external css
 const Footer=()=>{
+
+const [age, setage]=useState(18);
+console.log(age);
+
     return (
     <footer className="footer">
     <h1>Footer</h1>
+    <h1>{age}</h1>
+     <button className='bodyButton' onClick={
+        ()=>{
+        // age=20;
+        setage(20);
+    }
+    }>Click me</button>
+    {console.log("component re-rendering")}
     </footer>
     );
 }
