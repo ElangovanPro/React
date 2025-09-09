3e54ab93e525cdbac1525e02358990b260b6fc7c
ElangovanPro <elangovanvcet@gmail.com>
2025-09-09 14:01:55 -0500

    type and spread, rest operator typescript

---
+++ App.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/components/Header/Header";
+import Body from "./src/components/Body/Body";
+import Footer from "./src/components/Footer/Footer";
+import { Age } from "./src/components/Age/Age";
+import { Employee } from "./src/components/Employee/Employee";
+
+const AppLayout =()=>{
+   
+//   //spread operator - array
+//    const arr1=[1,2,3,4,5,6,7,8,8,9,9];
+//    const arr2=[4,5,6];
+
+//    const mergeArr=[arr1,arr2];
+//    const mergeAr1=[1,2,3,4,5,6,7,8,8,9,9,arr2];
+//    const mergeAr2=[...arr1,...arr2];
+
+//    console.log(mergeArr);
+//    console.log(mergeAr1);
+//    console.log(mergeAr2);
+
+//spread= object
+// const obj1={
+//     name:"sanjay",
+//     age:24,
+//   }
+// const obj2={
+//     city:"chennai",
+//     country:"India",
+//   }
+// const mergObj={obj1,obj2};
+//  const mergObj1={...obj1,...obj2};
+//  console.log(mergObj);
+//  console.log(mergObj1);
+
+// let num:number=10;
+
+// num="elango";
+
+
+//datatypes---number,boolean,string,undefined,null,unknown
+ const emp1:EmployeeProps={
+    name:"sanjay",
+    age:24,
+    city:"chennai",
+    country:"India",
+ }
+
+    return (
+    <>
+    {/* <Header />
+    <Body/>
+    <Footer/> */}
+    <Age/>
+    <Employee name="sanjay" age={24} city="chennai" country="India"/>
+    <Employee {...emp1} />
+    </>
+);
+}
+
+const root=ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<AppLayout/>);
---
+++ src/components/Employee/Employee.tsx
@@ -0,0 +1,60 @@
+// export const Employee=({name,age})=>{
+//     return (
+//     <>
+//     <h1>{name}</h1>
+//     <h1>{age}</h1>
+//     </>
+//     );
+// }
+
+// export const Employee=(props)=>{
+//     return (
+//     <>
+//     <h1>{props.name}</h1>
+//     </>
+//     );
+// }
+
+
+// export const Employee=(props)=>{
+//     return (
+//     <>
+//     <h1>{props.name}</h1>
+//     <h1>{props.age}</h1>
+//     <h1>{props.city}</h1>
+//     <h1>{props.country}</h1>
+//     </>
+//     );
+// }
+
+// let num:number=10;
+// num="efef";
+
+// export const Employee=({...rest})=>{
+//     return (
+//     <>
+//     <h1>{rest.name}</h1>
+//     {rest.name=333}
+//     <h1>{rest.name}</h1>
+//     <h1>{JSON.stringify(rest)}</h1>
+//     <h1>{rest.age}</h1>
+//     <h1>{rest.city}</h1>
+//     </>
+//     );
+// }
+
+
+
+
+export const Employee=({...rest}:EmployeeProps)=>{
+    return (
+    <>
+    <h1>{rest.name}</h1>
+    {rest.name=333}
+    <h1>{rest.name}</h1>
+    <h1>{JSON.stringify(rest)}</h1>
+    <h1>{rest.age}</h1>
+    <h1>{rest.city}</h1>
+    </>
+    );
+}
