4a24d3ad9b2ff80f059b2dcdbca8854b1a2977c2
ElangovanPro <elangovanvcet@gmail.com>
2025-09-30 14:01:19 -0500

    lodash object utility functions 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,68 +2,110 @@
 import { URL1, URL2 } from "../constants/constants";
 import { useState } from "react";
 import _ from "lodash";
-//on the fly destructing
+
 const Header=()=>{
 //JS functions---- Lodash utility functions - part 1..Array, object, collections, strings
 
 //syntax
-//1. get(object, path ("") ,defaultValue)
+//4. pick(object, [path])
 
 // const user={
-//   profile:{
-//     name:"divya",
-//     address:{
-//       city:"tirupur",
-//       state:"tamilnadu",
-//       district:"new yrok",
-//       country:"India"
-//     }
-//   }
+//   id:1,
+//   name:"sanjay",
+//   age:24,
+//   role:"tester"
 // };
 
-// const city=_.get(user,"profile.address", "there is no city");
-
-// console.log(user);
-// console.log(user.profile);
-// console.log(user.profile.name);
+// const payload=_.pick(user, ["id", "name"]);
 
 
-
-//2. set(object, path ("") ,defaultValue)
-
-// const [user, setuser]=useState({
-//   profile:{
-//     name:"divya"
-//   }
-// });
-
-// // console.log("user objec"+JSON.stringify(user));
-// const updateName=()=>{
-//   const updatedUser={...user};
-//   // console.log("updated user objec"+JSON.stringify(updatedUser));
-//   _.set(updatedUser,"profile.name","Elango");
-//   setuser(updatedUser);
-// }
-// console.log("user objec"+JSON.stringify(user));
-
-//3. has(object, path)
+//5. omit(object, [path])
 
 // const user={
-//   profile:{
-//    premium:true
-//   }
-// }
+//   id:1,
+//   name:"sanjay",
+//   age:24,
+//   role:"tester",
+//   password:"sangavi"
+// };
+
+// const payload=_.omit(user, ["password"]);
+
+//6.keys
+
+// const user={
+//   id:1,
+//   name:"sanjay",
+//   age:24,
+//   role:"tester",
+//   password:"sangavi"
+// };
+
+// const payload=_.keys(user);
+
+
+// 7. values
+
+
+// const user={
+//   id:1,
+//   name:"sanjay",
+//   age:24,
+//   role:"tester",
+//   password:"sangavi"
+// };
+
+// const payload=_.values(user);
+
+// map function
+
+// const numbers=[1,2,3,4,5];
+// const doubleNumbers= numbers.map((n)=>n/2);
+// console.log(doubleNumbers);
+
+
+//8.mapkeys(object, iteratee)
+
+// const user={
+//   id:1,
+//   name:"sanjay",
+// };
+
+// const payload=_.mapKeys(user, (value, key)=>`user_${key}`);
+
+
+//9. mapvalues(object, iteratee)
+
+// const user={
+//   science:"50",
+//   social:"100",
+// };
+
+// const payload=_.mapValues(user, (n)=>`user = ${n*2}`);
+
+
+//10 merge(object,sources)
+// const user1={
+//   id:1,
+//   name:"sanjay",
+//   age:44,
+//   salary:4000,
+// };
+
+// const user2={
+//   id:2,
+//   name:"jagadish",
+//   age:34,
+// };
+
+// const payload=_.merge({},user1,user2);
 
     return (
     <>
-    {/* {
-    _.has(user, "profile.premium") ? (<p>
-      premium user
-    </p>):(<p> Not a premium user </p>)
-  } */}
+     {/* <h1>{JSON.stringify(user)}</h1> */}
+     <p>{JSON.stringify(payload)}</p>
     </>
     );
-
 }
 
 export default Header;
