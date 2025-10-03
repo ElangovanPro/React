4b39a9922303d6d93682f76a9ddd9a7db7bd0900
ElangovanPro <elangovanvcet@gmail.com>
2025-10-03 13:55:47 -0500

    lodash object utility functions 3 with shallow comparison

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,104 +6,54 @@ import _ from "lodash";
 const Header=()=>{
 //JS functions---- Lodash utility functions - part 1..Array, object, collections, strings
 
-//syntax
-//4. pick(object, [path])
+//primitive values---Numbers, true, false, string
+   const [count,setcount]=useState(0);
+   
+   const [user,setuser]=useState({name:"Elango"});
 
-// const user={
-//   id:1,
-//   name:"sanjay",
-//   age:24,
-//   role:"tester"
-// };
+   const obj1={
+    id:1,
+    name:"elango",
+    address:{
+        city:"erode",
+        pincode:"638003"
+    }
+   };
+   const obj2={
+    id:1,
+    name:"elango",
+    address:{
+        city:"chennai",
+    }
+   };
 
-// const payload=_.pick(user, ["id", "name"]);
+const merged=_.merge({},obj1,obj2);
+console.log("merged"+JSON.stringify(merged));//overwrites with keep original
 
-
-//5. omit(object, [path])
-
-// const user={
-//   id:1,
-//   name:"sanjay",
-//   age:24,
-//   role:"tester",
-//   password:"sangavi"
-// };
-
-// const payload=_.omit(user, ["password"]);
-
-//6.keys
-
-// const user={
-//   id:1,
-//   name:"sanjay",
-//   age:24,
-//   role:"tester",
-//   password:"sangavi"
-// };
-
-// const payload=_.keys(user);
+const assign=_.assign({},obj1,obj2);
+console.log("merged"+JSON.stringify(assign));//overwrites without keep original
 
 
-// 7. values
 
-
-// const user={
-//   id:1,
-//   name:"sanjay",
-//   age:24,
-//   role:"tester",
-//   password:"sangavi"
-// };
-
-// const payload=_.values(user);
-
-// map function
-
-// const numbers=[1,2,3,4,5];
-// const doubleNumbers= numbers.map((n)=>n/2);
-// console.log(doubleNumbers);
-
-
-//8.mapkeys(object, iteratee)
-
-// const user={
-//   id:1,
-//   name:"sanjay",
-// };
-
-// const payload=_.mapKeys(user, (value, key)=>`user_${key}`);
-
-
-//9. mapvalues(object, iteratee)
-
-// const user={
-//   science:"50",
-//   social:"100",
-// };
-
-// const payload=_.mapValues(user, (n)=>`user = ${n*2}`);
-
-
-//10 merge(object,sources)
-// const user1={
-//   id:1,
-//   name:"sanjay",
-//   age:44,
-//   salary:4000,
-// };
-
-// const user2={
-//   id:2,
-//   name:"jagadish",
-//   age:34,
-// };
-
-// const payload=_.merge({},user1,user2);
-
+//    console.log("App rendereding"+count);
     return (
     <>
-     {/* <h1>{JSON.stringify(user)}</h1> */}
-     <p>{JSON.stringify(payload)}</p>
+     {count}
+     {user.name}
+     <button
+onClick={()=>{
+    setcount(pre=>pre+1);
+}}
+     >Click me for count</button>
+     {/* {    console.log(user)} */}
+          <button
+onClick={()=>{
+    user.name="jagadish"
+    // setuser({...user});//creating new object one of way
+    setuser(_.assign({},user,{name:"jagadish"}));//creating new object other one of way
+    console.log(user)
+}}
+     >Click me for user</button>
     </>
     );
 }
