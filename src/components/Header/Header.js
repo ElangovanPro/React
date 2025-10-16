49ce6dc3a2f1c485e39792e224993234f57ad2fd
ElangovanPro <elangovanvcet@gmail.com>
2025-10-16 12:33:35 -0500

    useEffect in depth part 3

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,100 +1,74 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 const Header = () => {
- 
-  const [user,setuser]=useState([]);
+  const [userid,setuserid]=useState(1);
+  const [userorg,setuserorg]=useState([]);
+  const [user,setuser]=useState({});
   const [loading, setloading]=useState(true);
   const [error, seterror]=useState(null);
-
-
-  // useEffect(()=>{
-  //   fetch("https://dummyjson.com/users")
-  //   .then((res)=>{
-  //     if(!res.ok){
-  //       throw new Error("network error");
-  //     }
-  //     return res.json();
-  //   })
-  //   .then(data=>{
-  //     setuser(data.users);
-  //     setInterval(()=>{
-  //       setloading(false);
-  //     }, 5000);
-  
-  //   })
-  //   .catch((err)=>{
-  //     seterror(err.message);
-  //     setloading(false);
-  //   })
-  // },[]);
-  
-  // useEffect(()=>{
-  //   const fetchUsers= async()=>{
-  //      try{
-  //       const res= await fetch("https://dummyjson.com/users");
-               
-  //      console.log(res);
-  //        if(!res.ok){
-  //       throw new Error("network error");
-  //     }
-  //       const data= await res.json();
-  //       setuser(data.users);
-  //     }
-  //      catch(err){
-  //     seterror(err.message);
-  //     setloading(false);
-  //      }
-  //      finally{
-  //             setInterval(()=>{
-  //       setloading(false);
-  //     }, 5000);
-  //      }
-  //   }
-  //   fetchUsers();
-  // },[]);
-
+ 
 
     useEffect(()=>{
-    const fetchUsers= async()=>{
-       try{
-        const res= await axios.get("https://dummyjson.com/users");
-console.log(res);
-        setuser(res.data.users);
+    fetch("https://dummyjson.com/users")
+    .then((res)=>{
+      if(!res.ok){
+        throw new Error("network error");
       }
-       catch(err){
-      seterror(err.message);
-      setloading(false);
-       }
-       finally{
-        setInterval(()=>{
+      return res.json();
+    })
+    .then(data=>{
+      setuserorg(data.users);
+      setInterval(()=>{
         setloading(false);
       }, 5000);
-       }
-    }
-    fetchUsers();
+  
+    })
+    .catch((err)=>{
+      seterror(err.message);
+      setloading(false);
+    })
   },[]);
 
- if(loading)
- {
-return (<p>Loading......</p>);
- }
- if(error)
- {
-  return (<p>Error : {error}</p>);
- }
+
+  useEffect(()=>{
+    fetch(`https://dummyjson.com/users/${userid}`)
+    .then((res)=>{
+      if(!res.ok){
+        throw new Error("network error");
+      }
+      return res.json();
+    })
+    .then(data=>{
+      setuser(data);
+      setInterval(()=>{
+        setloading(false);
+      }, 5000);
+  
+    })
+    .catch((err)=>{
+      seterror(err.message);
+      setloading(false);
+    })
+  },[userid]);
+
 
   return (
     <>
-    <h1>User List</h1>
     <ul>
-      {user.map((user)=>(
         <li key={user.id}>
            {user.firstName}
             {user.lastName}
             ---{user.email}
         </li>
-      ))}
     </ul>
+    <select onChange={(e)=>setuserid(e.target.value)}>
+      {
+        userorg.map((user)=>(
+           <option key={user.id} value={user.id}>
+ user {user.id}
+      </option>
+        ))
+      }
+    </select>
     </>
   );
 };
