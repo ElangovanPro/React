d4060ded1b3eb803877677c49c1f08916375a456
ElangovanPro <elangovanvcet@gmail.com>
2025-10-31 14:15:59 -0500

    JS Prototype in depth part 2

--- src/components/JS/JsPrototypeExample.js
+++ src/components/JS/JsPrototypeExample.js
@@ -1,67 +1,93 @@
-      /* primitive values vs objects 
+   /* primitive values vs objects 
     primitive- string, number, boolean, undefined,null (immutable, stored directly, lightweight)--storing by value...call stack--quick access
    non-primitive- arrays,objects----storing by reference...heap memory--slower access
     */
 export const JsPrototypeExample=()=>{
 
-    class Person{
- 
-   constructor(name,age){
-    this.name=name;
-    this.age=age;
-   }
-
-   greeting(){
-    console.log(this.name," "+ this.age);
-   }
-
-    }
-
-  let person1=new Person("elango",26);
-  person1.greeting();
+//every javascript value (except null and undefined) is connected to a
+//prototype object a hidden internal link that gives it access to built in methods.
+  
+  
+//   null
+  
+// object                  ----parent (ancestor)
 
 
-    let name="jagadish";
-    let age=25;
-    
-    name[0]="B";
-    age+6;
-    
-    console.log(name[0],age+6);
-
-    console.log(name," "+age);
-    
-  //re assign
-  const newName=name +" sanjay";
-  console.log(newName);
-  console.log(name);
-    
-  //re assign
-    age=age+6;
-    console.log(age);
+// Array String Number Boolean     ---- children 
 
 
-    const user={name:"Elango"};
-    user.name="Divya";
+// []    "text"  123     true      -----grand children
 
-    const arr=[1,2,3];
-    arr.push(4);
-    
-    console.log(user.name, arr);
 
-  //call by value
-    let e=10;
-    let f=e;
-    let g=20;
-    console.log(e);
-    console.log(f);
-    console.log(g);
+// object.prototype
 
- //call by reference
-    let obj1={name:"divya"};
-    let obj2=obj1;
-    obj2.name="sangavi";
-    console.log(obj1.name);
+// Array.prototype String.prototype Number.prototype Boolean.prototype    
+  
+  // let name="elango";
+  //  let newName=name.toUpperCase();
+  //  console.log(newName)
+
+  //  let name1="jagadish";
+  //  let newName1=name1.toUpperCase();
+  //  console.log(newName1)
+
+//   let name3="jagadish"; 
+//     // let newName3=name3.toUpperCase();
+//     //__proto__. to uppercase
+//   // console.log(name3.__proto__);// String.prototype
+
+//    let num=98;//Number.prototype
+
+//    let boo=true// Boolean.prototype
+  
+//    let arr= [1,23]//Array.prototype
+
+
+//   console.log(Object.getPrototypeOf(num)===Number.prototype);
+//   console.log(Object.getPrototypeOf(name3)===String.prototype);
+//   console.log(Object.getPrototypeOf(boo)===Boolean.prototype);
+//   console.log(Object.getPrototypeOf(arr)===Array.prototype);
+
+
+//  const obj1={name:"elango"};
+//  console.log(Object.getPrototypeOf(obj1));
+//  console.log(Object.getPrototypeOf(obj1)===Object.prototype);
+  
+
+//   console.log(Number.prototype.__proto__);
+//   console.log(Object.prototype.__proto__);
+
+
+
+//arr - >  Array.prototype ---> object.prototype --->null
+const arr=[1,2,3];
+const arr1=[4,5,6];
+const arr2=[7,8,9];
+
+Array.prototype.first1= function (){
+  return this[0];
+}
+
+// Object.prototype.first1= function (){
+//   return this[0];
+// }
+
+console.log(arr.first1())
+console.log(arr1.first1())
+console.log(arr2.first1())
+
+
+
+const name="elango";
+
+const name1="elango1";
+
+String.prototype.random = function (){
+  return "jjj";
+}
+
+console.log(name.random())
+console.log(name1.random())
 
     return (
         <>
